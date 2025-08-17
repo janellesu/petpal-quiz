062e6a4f68388f089ae42c7e7a47f31179087d45
Janelle <janesu0892@gmail.com>
2025-08-17 10:32:33 -0500

    made scoring table

--- script.js
+++ script.js
@@ -21,8 +21,85 @@ document.addEventListener("DOMContentLoaded", () => {
             p.textContent = `${question}: ${answer}`;
             resultContainer.appendChild(p);
         }
-        // TODO: make results more user friendly
     })
 })
 
 // TODO: Match answers to pet rec and pet care
+
+let scores = {
+    //dogs
+    labrador: 0,
+    golden: 0,
+    frenchBulldog: 0,
+    germanShepherd: 0,
+    poodle: 0,
+    pom: 0,
+
+
+    //cats
+    persian: 0, //not active
+    siamese: 0, //active
+    maineCoon: 0, //active
+    exotic: 0, //active
+    american: 0, //not active
+
+
+    //other
+    rabbit: 0,
+    hamster: 0,
+    parrot: 0,
+    gecko: 0
+};
+
+const scoring = {
+    activity: {
+        "very-active": { largeDogs: 3, activeCats: 2},
+        "somewhat-active": { smallDogs: 3, otherPets: 1},
+        "not-active": { notActiveCats: 3, otherPets: 3}
+    },
+    home: {
+        "small-home": {smallDogs: 3, otherPets:3, notActiveCats: 3 },
+        "medium-home": { activeCats: 3, parrot: 3, otherPets: 2},
+        "big-home": { largeDogs: 3}
+    },
+    time: {
+        "very-little-time": {lowMaintenance: 3, modMaintenance: 1},
+        "enough-time": {highMaintenance: 1, modMaintenance: 3, lowMaintenance: 2},
+        "lots-of-time": {highMaintenance: 3, modMaintenance: 3, lowMaintenance: 3}
+    },
+    experience: {
+        "no-experience": {smallDogs: 3, otherPets: 3, notActiveCats: 3},
+        "some-experience": {smallDogs: 3, activeCats: 3, otherPets: 3, largeDogs: 2},
+        "experienced": {dogs: 3, cats: 3, otherPets: 3}
+    },
+    "pet-interest": {
+        "dog-interest": {dogs: 5},
+        "cat-interest": {cats: 5},
+        "other-interest": {otherPets: 5}
+    },
+    "grooming-importance": {
+        "low-maintenance": {lowMaintenance: 3},
+        "moderate-maintenance": {modMaintenance: 3, lowMaintenance: 2},
+        "high-maintenance": {highMaintenance: 3, modMaintenance: 2, lowMaintenance: 1}
+    },
+    "children-or-pets": {
+        "children-and-pets": {friendly: 3},
+        "children-or-other-pets": {friendly: 3},
+        "no-children-or-pets": {independent: 3, friendly: 2}
+    },
+    allergies: {
+        "allergic": {hypoallergenic: 3},
+        "not-allergic": {}
+
+    },
+    "pet-temper": {
+        "calm-pet": {calm: 3},
+        "playful-pet": {playful: 3},
+        "protective-pet": {protective: 3}
+    },
+    noise: {
+        "quiet-noise": {lowNoise: 3, mediumNoise: 2},
+        "moderate-noise": {lowNoise: 3, mediumNoise:3, noisy: 1},
+        "noisy": {noisy: 3, mediumNoise: 3, lowNoise: 3}
+    }
+};
